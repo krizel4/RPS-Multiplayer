d3ffbee7d355ad3ef4d6dbf4ffc4716c1a625518
Krizel <krizzlybear@Nathans-MacBook-Pro.local>
2019-11-19 19:11:22 -0800

    adding more specificity to the html page

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RPS-Multiplayer
 A simple rock paper scissors game to practice with Firebase.
+
+# How to play
+
+# Features
+
+# Built with
+* HTML5 
+* CSS
+* jQuery
+* Javascript
+* Firebase
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
 // ================
+// ASSIGNMENT INSTRUCTIONS
+// ================
+   
+   // * Only two users can play at the same time.
+
+    // * Both players pick either `rock`, `paper` or `scissors`. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+  
+    // * The game will track each player's wins and losses.
+  
+    // * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+  
+    // * Styling and theme are completely up to you. Get Creative!
+  
+    // * Deploy your assignment to Github Pages.
+
+// ================
 // FIREBASE
 // ================
 
@@ -31,16 +47,11 @@ var config = {
 
 $(document).ready(function(){
 
-    // * Only two users can play at the same time.
+    // User types in name
+    var playeroneName = document.getElementById("username").value;
 
-    // * Both players pick either `rock`, `paper` or `scissors`. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
-  
-    // * The game will track each player's wins and losses.
-  
-    // * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
-  
-    // * Styling and theme are completely up to you. Get Creative!
-  
-    // * Deploy your assignment to Github Pages.
+    
+    // Fetch names from Firebase
+
 
 });
