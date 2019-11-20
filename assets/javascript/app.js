f2d842ef9956cf72459b2c4109bf572708830cc9
Krizel <krizzlybear@Nathans-MacBook-Pro.local>
2019-11-19 17:35:42 -0800

    adding more to the html skeleton

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,46 @@
+// ================
+// FIREBASE
+// ================
+
+var config = {
+    apiKey: "AIzaSyD92XpkWmDFDAptaMNxaxkmLtBt88OwJeQ",
+    authDomain: "rps-multiplayer-4380b.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-4380b.firebaseio.com",
+    projectId: "rps-multiplayer-4380b",
+    storageBucket: "rps-multiplayer-4380b.appspot.com",
+  };
+
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+
+// ****** NEED TO DECIDE WHAT TYPE OF DATA TO STORE
+//   var playerone = 0;
+//   var playertwo = 0;
+//   $("#userChoice").on("click", function() {
+//       userChoice++; 
+//   });
+//   var chat;
+//   database.ref().set({
+//       usercount: userChoice
+//   })
+
+// ================
+// RPS GAME 
+// ================
+
 $(document).ready(function(){
 
+    // * Only two users can play at the same time.
+
+    // * Both players pick either `rock`, `paper` or `scissors`. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+  
+    // * The game will track each player's wins and losses.
+  
+    // * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+  
+    // * Styling and theme are completely up to you. Get Creative!
+  
+    // * Deploy your assignment to Github Pages.
 
 });
